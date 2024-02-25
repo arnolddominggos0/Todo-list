890ba6b3bd5e1f67e95280660727dceb99bf5110
Arnold Dominggos Sitompul <arnolddominggos@student.telkomuniversity.ac.id>
2024-02-25 14:52:49 +0700

    refactor input to custom component

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,6 +2,8 @@
 import { useListStore } from '@/stores/list'
 import { mapState, mapActions } from 'pinia'
 
+import BaseInput from '@/components/BaseInput.vue'
+
 // Initial input for the form
 const initialInput = {
   name: '',
@@ -11,9 +13,13 @@ const initialInput = {
 export default {
   name: 'ListView',
   data: () => ({
-    // Use the spread operator to create a copy of initialInput
     input: { ...initialInput }
   }),
+  // declate component
+  components: {
+    BaseInput
+  },
+
   computed: {
     // Import the getList state from the store
     ...mapState(useListStore, ['getList'])
@@ -22,19 +28,15 @@ export default {
     // Import the a$addList action from the store
     ...mapActions(useListStore, ['a$addList']),
 
-    // Method to add a new list
     addList() {
       // Check if the name is not empty or only contains spaces
       if (this.input.name && this.input.name.trim() !== '') {
-        // Call the a$addList action from the store
         this.a$addList({ ...this.input })
 
-        // Reset the name field to an empty string
         this.input.name = ''
       }
     },
 
-    // Method to handle form submission
     addForm(event) {
       // Log the event to the console
       console.log(event)
@@ -57,25 +59,20 @@ export default {
 <template>
   <div>
     <h1>List</h1>
-    
-    <!-- Form element with a submit event listener -->
+
     <form @submit.prevent="($event) => addForm($event)" method="post">
-      <!-- Input field for the name with v-model binding -->
-      <input class="input" v-model="input.name" type="text" placeholder="add new list" />
+      <!-- use component using kebab-case -->
+      <base-input v-model="input.name" class="input" placeholder="add new"></base-input>
       <br />
 
       <!-- Checkbox for completed with v-model binding -->
-      <input v-model="input.completed" type="checkbox" name="completed" id="completed" />
-      Completed
+      <input v-model="input.completed" type="checkbox" /> Completed
       <br />
 
-      <!-- Button to submit the form -->
       <button type="submit">Add</button>
     </form>
 
-    <!-- Ordered list to display the lists -->
     <ol class="list">
-      <!-- Loop through the lists and display each item in a list item -->
       <template v-for="(item, index) in getList" :key="index">
         <li>{{ item.name }}</li>
       </template>
@@ -84,23 +81,19 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-/* Styling for input fields */
 input {
   padding: 0.5rem;
   font-size: 1rem;
 }
 
-/* Styling for buttons */
 button {
   height: 2rem;
 }
 
-/* Styling for the container div */
 div {
   display: grid;
 }
 
-/* Styling for the list */
 .list {
   margin-block: 0.5rem;
 }
