56662e0b2700d3bde9be8c3e0658c231f43c0f85
Arnold Dominggos Sitompul <arnolddominggos@student.telkomuniversity.ac.id>
2024-02-25 13:48:39 +0700

    add list view

---
+++ src/views/ListView.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data: () => ({
+    input: {}
+  }),
+
+  methods: {
+    log() {
+      console.log(this.input)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="input.title" />
+
+    <button @click="log()">Log to Console</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+input {
+  height: 2rem;
+}
+button {
+    height: 2rem;
+}
+div {
+    display: grid;
+}
+</style>
